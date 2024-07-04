d48cb7eaa7ae7f7da9716f92874faf9a815bb985
Rudra Dey Sarkar <97943164+Rudra-Dey-Sarkar@users.noreply.github.com>
2024-07-04 15:04:53 +0530

    Update Facebook.js

--- src/Components/Facebook.js
+++ src/Components/Facebook.js
@@ -21,15 +21,6 @@ export default function Facebook() {
                 xfbml: true,
                 version: 'v20.0'
             });
-
-            // Load Facebook SDK asynchronously
-            (function (d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) return;
-                js = d.createElement(s); js.id = id;
-                js.src = "https://connect.facebook.net/en_US/sdk.js";
-                fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
         }
     }, []);
 
